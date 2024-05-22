98bbdb387d1889d11c4751e4de5e080095dcd3b6
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2024-05-23 00:08:04 +0300

    - Shows matrices on the page

--- src/main/java/com/example/MatrixController.java
+++ src/main/java/com/example/MatrixController.java
@@ -4,10 +4,7 @@ import com.example.fox.FoxAlgorithm;
 import com.example.shared.Matrix;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,26 +12,23 @@ import java.util.List;
 @RestController
 public class MatrixController {
 
+    int size = 3;
+    Matrix matrix1 = new Matrix(size, size);
+    Matrix matrix2 = new Matrix(size, size);
+
+    @CrossOrigin
     @GetMapping("/")
     public List<int[][]> showMatrices() {
-        int[][] matrix1 = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        int[][] matrix2 = {
-                {9, 8, 7},
-                {6, 5, 4},
-                {3, 2, 1}
-        };
-        return Arrays.asList(matrix1, matrix2);
+        matrix1.generateRandomMatrix();
+        matrix2.generateRandomMatrix();
+        return Arrays.asList(matrix1.matrix, matrix2.matrix);
     }
 
 
     @PostMapping("/multiply")
     public int[][] multiplyMatrices(@RequestBody MatrixPayload payload) {
-        Matrix firstMatrix = new Matrix(payload.getFirstMatrix());
-        Matrix secondMatrix = new Matrix(payload.getSecondMatrix());
+        Matrix firstMatrix = payload.getFirstMatrix();
+        Matrix secondMatrix = payload.getSecondMatrix();
         FoxAlgorithm algorithm = new FoxAlgorithm(payload.getThreadCount(), firstMatrix, secondMatrix);
         return algorithm.algorithm();
     }
@@ -42,8 +36,8 @@ public class MatrixController {
     @Setter
     @Getter
     public static class MatrixPayload {
-        private int[][] firstMatrix;
-        private int[][] secondMatrix;
+        private Matrix firstMatrix;
+        private Matrix secondMatrix;
         private int threadCount;
 
     }
